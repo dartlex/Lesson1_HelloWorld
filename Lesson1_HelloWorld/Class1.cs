dca94f76bb13ce03e0823a2f143fa87afe9d7193
dartlex <dartlex.rmnv@gmail.com>
2026-06-10 21:55:05 +0300

    Add project files.

---
+++ Lesson1_HelloWorld/Class1.cs
@@ -0,0 +1,13 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace Lesson1_HelloWorld
+{
+    public class StartCommand : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
